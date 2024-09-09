1ccd6ceea47d43c4170ccd5cd262f90d5aa9e41b
Arseniy Kuznetsov <Kuarni22@gmail.com>
2024-09-09 15:24:01 +0300

    task-2: add graph_to_nfa

--- project/task_2_dfa_and_nfa.py
+++ project/task_2_dfa_and_nfa.py
@@ -1,7 +1,21 @@
-from pyformlang.finite_automaton import DeterministicFiniteAutomaton
+from typing import Set
+from networkx import MultiDiGraph
+from pyformlang.finite_automaton import (
+    DeterministicFiniteAutomaton,
+    NondeterministicFiniteAutomaton,
+)
 from pyformlang.regular_expression import Regex
 
 
 def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
     regex = Regex(regex)
     return regex.to_epsilon_nfa().to_deterministic()
+
+
+def graph_to_nfa(
+    graph: MultiDiGraph, start_states: Set[int], final_states: Set[int]
+) -> NondeterministicFiniteAutomaton:
+    nfa = NondeterministicFiniteAutomaton.from_networkx(graph)
+    nfa.start_states = start_states
+    nfa.final_states = final_states
+    return nfa
