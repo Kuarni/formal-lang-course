38d1a510768d976d5964ad5ace1f5e62aaa34c65
Arseniy Kuznetsov <Kuarni22@gmail.com>
2024-09-13 23:23:12 +0300

    task-2: add tests for graph_to_nfa

--- tests/test_task_2_dfa_and_nfa.py
+++ tests/test_task_2_dfa_and_nfa.py
@@ -1,7 +1,11 @@
+import os
+import shutil
+import networkx as nx
 from collections.abc import Iterable
-from pyformlang.finite_automaton import Symbol
+from pyformlang.finite_automaton import Symbol, NondeterministicFiniteAutomaton, State
 
-from project.task_2_dfa_and_nfa import regex_to_dfa
+from project.task_1_graph_utils import get_graph, create_and_save_graph
+from project.task_2_dfa_and_nfa import regex_to_dfa, graph_to_nfa
 
 
 class TestRegexToDfa:
@@ -71,3 +75,75 @@ class TestRegexToDfa:
         dfa = regex_to_dfa("i*.s (m.i)|(n.i)+m.u.m.$")
         min_dfa = dfa.minimize()
         assert dfa == min_dfa
+
+
+class TestGraphToNfa:
+    # teardown removes all files in this directory
+    results_path = "test_2_data"
+
+    def get_graph_path(self, graph_name) -> str:
+        return os.path.join(self.results_path, graph_name + ".dot")
+
+    def test_is_nfa(self):
+        graph = get_graph("skos")
+        nfa = graph_to_nfa(graph)
+        assert not nfa.is_deterministic()
+
+    @staticmethod
+    def get_expected_nfa_for_tests(start_states, final_states):
+        expected_nfa = NondeterministicFiniteAutomaton()
+        for i in start_states:
+            expected_nfa.add_start_state(State(i))
+        for i in final_states:
+            expected_nfa.add_final_state(State(i))
+
+        expected_nfa.add_transition(State(1), Symbol("a"), State(0))
+        expected_nfa.add_transition(State(0), Symbol("a"), State(1))
+        expected_nfa.add_transition(State(2), Symbol("b"), State(0))
+        expected_nfa.add_transition(State(0), Symbol("b"), State(2))
+        return expected_nfa
+
+    def test_nfa_no_final_start_state(self):
+        graph_path = self.get_graph_path("graph_1")
+        create_and_save_graph(1, 1, ("a", "b"), graph_path)
+
+        graph = nx.drawing.nx_pydot.read_dot(graph_path)
+        nfa = graph_to_nfa(graph)
+
+        assert len(nfa.start_states) == len(nfa.states)
+        assert len(nfa.final_states) == len(nfa.states)
+
+        expected_nfa = self.get_expected_nfa_for_tests({0, 1, 2}, {0, 1, 2})
+        assert nfa.is_equivalent_to(expected_nfa)
+
+    def test_nfa_with_final_start_states(self):
+        graph_path = self.get_graph_path("graph_2")
+        create_and_save_graph(1, 1, ("a", "b"), graph_path)
+
+        graph = nx.drawing.nx_pydot.read_dot(graph_path)
+        nfa = graph_to_nfa(graph, {1}, {2})
+
+        assert len(nfa.start_states) == 1
+        assert len(nfa.final_states) == 1
+
+        expected_nfa = self.get_expected_nfa_for_tests({1}, {2})
+        assert nfa.is_equivalent_to(expected_nfa)
+
+    def test_nfa_with_several_start_states(self):
+        graph_path = self.get_graph_path("graph_3")
+        create_and_save_graph(1, 1, ("a", "b"), graph_path)
+
+        graph = nx.drawing.nx_pydot.read_dot(graph_path)
+        nfa = graph_to_nfa(graph, {1, 0}, {2})
+
+        assert len(nfa.start_states) == 2
+        assert len(nfa.final_states) == 1
+
+        expected_nfa = self.get_expected_nfa_for_tests({1, 0}, {2})
+        assert nfa.is_equivalent_to(expected_nfa)
+
+    def setup_class(self):
+        os.mkdir(self.results_path)
+
+    def teardown_class(self):
+        shutil.rmtree(self.results_path)
