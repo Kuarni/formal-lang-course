4af2e7bc67ad99ba77f90150eade6b2359d10c20
Arseniy Kuznetsov <Kuarni22@gmail.com>
2024-10-13 17:38:30 +0300

    task-5: add matrix_type to task4 ms_bfs_based_rpq

--- project/task4.py
+++ project/task4.py
@@ -11,15 +11,16 @@ from project.task3 import AdjacencyMatrixFA
 
 
 class MsBfsRpq:
+    __matrix_type: type(sp.spmatrix)
     __adj_dfa: AdjacencyMatrixFA
     __adj_nfa: AdjacencyMatrixFA
     __shift: int
     __intersected_symbols: set[Symbol]
-    __adj_united: Dict[Symbol, sp.spmatrix]
-    __init_front: sp.spmatrix
-    __front_left: sp.spmatrix
+    __adj_united: Dict[Symbol, type(sp.spmatrix)]
+    __init_front: type(sp.spmatrix)
+    __front_left: type(sp.spmatrix)
 
-    def __unite_matrices(self) -> dict[Symbol, sp.spmatrix]:
+    def __unite_matrices(self) -> dict[Symbol, type(sp.spmatrix)]:
         return {
             symbol: sp.block_diag(
                 (
@@ -30,12 +31,12 @@ class MsBfsRpq:
             for symbol in self.__intersected_symbols
         }
 
-    def __get_init_front(self) -> sp.spmatrix:
+    def __get_init_front(self) -> type(sp.spmatrix):
         left_vector = sp.identity(self.__shift, dtype=bool)
 
         vectors = []
         for nfa_state_num in range(len(self.__adj_nfa.start_states)):
-            right_vector = sp.lil_matrix(
+            right_vector = self.__matrix_type(
                 (self.__shift, self.__adj_nfa.states_number), dtype=bool
             )
             for i in self.__adj_dfa.start_states:
@@ -43,9 +44,17 @@ class MsBfsRpq:
 
             vectors.append(sp.block_array([[left_vector, right_vector]]))
 
-        return reduce(lambda a, b: sp.block_array([[a], [b]]), vectors).tocsr()
+        return self.__matrix_type(
+            reduce(lambda a, b: sp.block_array([[a], [b]]), vectors)
+        )
 
-    def __init__(self, adj_dfa: AdjacencyMatrixFA, adj_nfa: AdjacencyMatrixFA):
+    def __init__(
+        self,
+        adj_dfa: AdjacencyMatrixFA,
+        adj_nfa: AdjacencyMatrixFA,
+        matrix_type=sp.csr_matrix,
+    ):
+        self.__matrix_type = matrix_type
         self.__adj_dfa = adj_dfa
         self.__adj_nfa = adj_nfa
         self.__start_states_list = list(adj_nfa.start_states)
@@ -57,10 +66,10 @@ class MsBfsRpq:
         self.__init_front = self.__get_init_front()
         self.__front_left = self.__init_front[:, : self.__shift]
 
-    def __update_front(self, front_right: sp.spmatrix) -> sp.lil_matrix:
-        def front_mul_matrix(cur_front, matrix) -> sp.lil_matrix:
-            mul = sp.csr_matrix(cur_front @ matrix)
-            diag_front_right = sp.lil_matrix(front_right.shape, dtype=bool)
+    def __update_front(self, front_right: sp.spmatrix) -> type(sp.spmatrix):
+        def front_mul_matrix(cur_front, matrix) -> type(sp.spmatrix):
+            mul = self.__matrix_type(cur_front @ matrix)
+            diag_front_right = self.__matrix_type(front_right.shape, dtype=bool)
 
             for i, j in zip(*mul[:, : self.__shift].nonzero()):
                 diag_front_right[i // self.__shift * self.__shift + j, :] += mul[
@@ -72,12 +81,12 @@ class MsBfsRpq:
         updated_front = reduce(
             lambda vector, matrix: vector + front_mul_matrix(front, matrix),
             self.__adj_united.values(),
-            sp.lil_matrix(front_right.shape, dtype=bool),
+            self.__matrix_type(front_right.shape, dtype=bool),
         )
 
         return updated_front
 
-    def __visited_to_dict(self, visited: sp.csr_matrix) -> dict[int, set[int]]:
+    def __visited_to_dict(self, visited: sp.spmatrix) -> dict[int, set[int]]:
         start_to_reachable = defaultdict(set)
         for start_state, final_state in product(
             range(len(self.__start_states_list)), self.__adj_dfa.final_states
@@ -91,8 +100,10 @@ class MsBfsRpq:
         return start_to_reachable
 
     def __ms_bfs(self) -> dict[int, set[int]]:
-        front_right = sp.lil_matrix(self.__init_front[:, self.__shift :], dtype=bool)
-        visited = sp.csr_matrix(front_right, dtype=bool)
+        front_right = self.__matrix_type(
+            self.__init_front[:, self.__shift :], dtype=bool
+        )
+        visited = self.__matrix_type(front_right, dtype=bool)
 
         while front_right.count_nonzero():
             front_right = self.__update_front(front_right)
@@ -106,14 +117,18 @@ class MsBfsRpq:
 
 
 def ms_bfs_based_rpq(
-    regex: str, graph: MultiDiGraph, start_nodes: set[int], final_nodes: set[int]
+    regex: str,
+    graph: MultiDiGraph,
+    start_nodes: set[int],
+    final_nodes: set[int],
+    matrix_type=sp.csr_matrix,
 ) -> set[tuple[int, int]]:
     regex_dfa = regex_to_dfa(regex)
-    adj_dfa = AdjacencyMatrixFA(regex_dfa)
+    adj_dfa = AdjacencyMatrixFA(regex_dfa, matrix_type)
     graph_nfa = graph_to_nfa(graph, start_nodes, final_nodes)
-    adj_nfa = AdjacencyMatrixFA(graph_nfa)
+    adj_nfa = AdjacencyMatrixFA(graph_nfa, matrix_type)
 
-    result = MsBfsRpq(adj_dfa, adj_nfa)()
+    result = MsBfsRpq(adj_dfa, adj_nfa, matrix_type)()
 
     retrieved_states = {
         (start, final)
